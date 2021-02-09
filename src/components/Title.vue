5a319abe15c12238de8bea0c9fb1534d3320d344
oherna20 <orazalhernandez.oh@gmail.com>
2021-02-09 17:01:57 -0600

    Scafold all the things

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+   <h1 class="text-6xl font-light text-gray-800 text-center">
+       My To Do App!
+    </h1> 
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+    msg: {
+        type: String,
+        default: 'Custom Tite',
+    },
+})
+</script>
